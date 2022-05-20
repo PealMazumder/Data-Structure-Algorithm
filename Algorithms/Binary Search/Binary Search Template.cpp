40b92d0f482a7a95b778ea1ebdfa267238a514f8
Peal Mazumder <33184874+PealMazumder@users.noreply.github.com>
2022-05-20 20:43:21 +0600

    Update Binary Search Template.cpp

--- Algorithms/Binary Search/Binary Search Template.cpp
+++ Algorithms/Binary Search/Binary Search Template.cpp
@@ -1,7 +1,7 @@
 ll Binay_Search(ll l, ll r, ll x)
 {
     while(l <= r)
-	{
+    {
         ll mid = (l + r)/2;
         if(ar[mid] == x) return mid;
         else if(ar[mid] < x) l = mid+1;
@@ -14,7 +14,7 @@ ll Binay_Search(ll l, ll r, ll x)
 ll Lower_Bound(ll l, ll r, ll x)
 {
     while(l < r)
-	{
+    {
         ll mid = (l + r)/2;
         if(ar[mid] >= x) r = mid;
         else l = mid + 1;
@@ -22,6 +22,16 @@ ll Lower_Bound(ll l, ll r, ll x)
     return l;
 }
 
+ll Upper_Bound(ll l, ll r, ll x)
+{
+    while(l < r)
+    {
+        ll mid = (l + r)/2;
+        if(ar[mid] > x) r = mid;
+        else l = mid + 1;
+    }
+    return l;
+}
 
 ll Upper_Bound(ll l, ll r, ll x)
 {
